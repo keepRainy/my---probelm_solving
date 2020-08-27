75d6edf21bc8496ebe6073d688eeb31b3a92f74b
Rainy <abyss7500@snu.ac.kr>
2020-08-27 15:05:42 +0900

    Code / BOJ :: 5525

---
+++ BOJ/Archive/5525/5525_Rainy.cpp
@@ -0,0 +1,51 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by Rainy
+//
+//  Open Source
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+
+// Set up : Functions Declaration
+/* None */
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N; cin >> N;
+    int M; cin >> M;
+    string S; cin >> S;
+
+    // Process
+    int dp[M];
+    memset(dp, 0, sizeof(dp));
+    int ans = 0;
+    for (int i=2; i<M; i++) {
+        if (S[i] == 'I' && S[i-1] == 'O' && S[i-2] == 'I') {
+            dp[i] = dp[i-2] + 1;
+            if (dp[i] >= N) {
+                ans++;
+            }
+        }
+    }
+
+    // Control : Output
+    cout << ans << endl;
+}
+
+// Helper Functions
+/* None */
