b7b3af1a61ee5e4498b3ca25d4fb54074ae4e612
Rainy <abyss7500@snu.ac.kr>
2020-08-30 15:52:47 +0900

    Code / BOJ :: 11058

---
+++ BOJ/Archive/11058/11058_Rainy_DynamicProgramming_2.cpp
@@ -0,0 +1,50 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by Rainy
+//
+//  Open Source
+
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+/* None */
+
+// Set up : Functions Declaration
+/* None */
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N; cin >> N;
+
+    // Process
+    long dp[N+1];
+
+    for (int i=1; i<=N; i++) {
+        dp[i] = i;
+    }
+
+    for (int i=1; i<=N-3; i++) {
+        for (int j=i+3; j<=N; j++) {
+            dp[j] = max(dp[j], dp[i]*(j-i-1));
+        }
+    }
+
+    // Control : Output
+    cout << dp[N] << endl;
+}
+
+// Helper Functions
+/* None */
+
