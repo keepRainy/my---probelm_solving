0d4b15ad1fbc8e4da262743571cd59aa68c4a935
Rainy <abyss7500@snu.ac.kr>
2020-09-20 15:48:20 +0900

    Code / BOJ :: 16936

---
+++ BOJ/Archive/16936/16936_Rainy.cpp
@@ -0,0 +1,69 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by Rainy
+//
+//  Open Source
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+/* None */
+
+// Set up : Functions Declaration
+bool comp(long u, long v);
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N; cin >> N;
+    long A[N];
+    for (int i=0; i<N; i++) {
+        cin >> A[i];
+    }
+
+    // Process
+    sort(A, A+N, comp);
+
+    // Control : Output
+    for (int i=0; i<N; i++) {
+        cout << A[i] << ' ';
+    }
+}
+
+// Helper Functions
+bool comp(long u, long v)
+{
+    int u2 = 0, u3 = 0;
+    while (u%2 == 0) {
+        u2++;
+        u /= 2;
+    }
+    while (u%3 == 0) {
+        u3++;
+        u /= 3;
+    }
+
+    int v2 = 0, v3 = 0;
+    while (v%2 == 0) {
+        v2++;
+        v /= 2;
+    }
+    while (v%3 == 0) {
+        v3++;
+        v /= 3;
+    }
+
+    return make_pair(u3, -u2) > make_pair(v3, -v2);
+}
