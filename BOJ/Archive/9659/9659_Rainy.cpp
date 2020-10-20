6b369ab7e7926e26ed0f87727d426ebe1aa677eb
Rainy <abyss7500@snu.ac.kr>
2020-10-21 00:44:14 +0900

    Code / BOJ :: 9659

---
+++ BOJ/Archive/9659/9659_Rainy.cpp
@@ -0,0 +1,37 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by Rainy
+//
+//  Open Source
+
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+/* None */
+
+// Set up : Functions Declaration
+/* None */
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    long N; cin >> N;
+
+    // Control : Output
+    cout << ((N%2) ? "SK" : "CY") << endl;
+}
+
+// Helper Functions
+/* None */
+
