55e1d67d7c5302f96a873775b4e2d3bada08a604
Rainy <abyss7500@snu.ac.kr>
2020-11-17 19:55:34 +0900

    Code / BOJ :: 7570

---
+++ BOJ/Archive/7570/7570_Rainy.cpp
@@ -0,0 +1,51 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by Rainy
+//
+//  Open Source
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+/* None */
+
+// Set up : Functions Declaration
+/* None */
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N; cin >> N;
+    int A[N];
+    for (int i=0; i<N; i++)
+        cin >> A[i];
+
+    // Process
+    int dp[N+1];
+    memset(dp, 0, sizeof(dp));
+
+    int mx = -1;
+    for (int i=0; i<N; i++) {
+        dp[A[i]] = dp[A[i]-1] + 1;
+        mx = max(mx, dp[A[i]]);
+    }
+
+    // Control : Output
+    cout << N-mx << endl;
+}
+
+// Helper Functions
+/* None */
+
