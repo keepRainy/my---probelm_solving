1e21bd8c20ba0734fe15541eb887cd240ce103f2
Rainy <abyss7500@snu.ac.kr>
2020-12-20 11:08:18 +0900

    Code / BOJ :: 2548

---
+++ BOJ/Archive/2548/2548_GGlifer.cpp
@@ -0,0 +1,44 @@
+//
+//  BOJ
+//  ver.C++
+//
+//  Created by GGlifer
+//
+//  Open Source
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define endl '\n'
+
+// Set up : Global Variables
+/* None */
+
+// Set up : Functions Declaration
+/* None */
+
+
+int main()
+{
+    // Set up : I/O
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Set up : Input
+    int N; cin >> N;
+    int A[N];
+    for (int i=0; i<N; i++)
+        cin >> A[i];
+
+    // Process
+    sort(A, A+N);
+
+    // Control : Output
+    cout << A[(N-1)/2] << endl;
+}
+
+// Helper Functions
+/* None */
+
